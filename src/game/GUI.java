70ceababf54f80bf42e0f30aebe724454ed1aad6
Delons <delonsiv@gmail.com>
2013-10-11 15:59:33 -0400

    Created GUI

--- src/game/GUI.java
+++ src/game/GUI.java
@@ -1,5 +1,29 @@
 package game;
 
+import javax.swing.*;
+
+import java.awt.*;
+
 public class GUI {
+
+	public static GUI instance;
+	
+	public static GUI getInstance() {
+		
+		if (instance == null) {
+			instance = new GUI();
+		}
+		return instance;
+		
+	}
+	
+	JFrame f = new JFrame("Timeline");
+	
+	public void drawGUI() {
+
+		f.setSize(500,500);
+		f.setVisible(true);
+		
+	}
 
 }
--- src/game/Timeline.java
+++ src/game/Timeline.java
@@ -2,4 +2,35 @@ package game;
 
 public class Timeline {
 
+	public static void main(String[] args) {
+
+		new Timeline().init();
+		
+	}
+
+	public static Timeline instance;
+
+	public static Timeline getInstance() {
+
+		if (instance == null) {
+			instance = new Timeline();
+		}
+		return instance;
+
+	}
+
+	String version = "V 1.0.0.0_0";
+	
+	private GUI gui = GUI.getInstance();
+	
+	public void init() {
+
+		gui.drawGUI();
+		
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
 }
